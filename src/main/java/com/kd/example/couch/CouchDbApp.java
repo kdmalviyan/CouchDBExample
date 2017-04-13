fc324f6b8abd5773667b7a543f359a7659708757
Kuldeep Singh <kdmalviyan@gmail.com>
2017-04-13 13:58:40 +0530

    Adding Logger

--- src/main/java/com/kd/example/couch/CouchDbApp.java
+++ src/main/java/com/kd/example/couch/CouchDbApp.java
@@ -3,6 +3,8 @@ package com.kd.example.couch;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.document.JsonDocument;
 import com.couchbase.client.java.document.json.JsonObject;
@@ -11,16 +13,20 @@ import com.kd.example.couch.service.UserService;
 
 public class CouchDbApp {
 	static String[] nodes = { "localhost", "127.0.0.1" };
-	
-	//create new bucket if not exists
+	static Logger logger = Logger.getLogger(CouchDbApp.class);
+	// create new bucket if not exists
 	static boolean createnew = true;
 	static Bucket bucket = ConnectionManager.getCouchbaseClient("CRUD_BUCKET", createnew, nodes);
 
 	public static void main(String[] args) throws InterruptedException {
+		logger.info("Creating dummy document for inserting into couch bucket.");
 		JsonObject data = JsonObject.create().put("type", "user").put("name", "kuldeep").put("password", "password");
+		logger.info("Creating location, inner document...");
 		JsonObject location = JsonObject.create().put("Flat", "CG1 403").put("Locality", "Supertech").put("Sector", 74)
 				.put("city", "Noida");
 		data.put("location", location);
+
+		logger.info("Creating service instance for communicating with couchbase...");
 		UserService service = new UserService(bucket);
 
 		// JsonDocument document = JsonDocument.create("user::kdmalviyan",
@@ -35,18 +41,21 @@ public class CouchDbApp {
 		// service.deleteUserInfo(document);
 
 		// service.deleteAllUsers();
-		/* int docsToCreate = 10;
-		 List<JsonDocument> documentList = new ArrayList<JsonDocument>();
-
-		 for (int i = 0; i < docsToCreate; i++) {
-		 documentList.add(JsonDocument.create("doc-" + i, data));
-		 }
-
-		 service.bulkCreate(documentList);*/
-
+		/*
+		 * int docsToCreate = 10; List<JsonDocument> documentList = new
+		 * ArrayList<JsonDocument>();
+		 * 
+		 * for (int i = 0; i < docsToCreate; i++) {
+		 * documentList.add(JsonDocument.create("doc-" + i, data)); }
+		 * 
+		 * service.bulkCreate(documentList);
+		 */
+		logger.info("gettiong all the documents from bucket.");
 		List<JsonDocument> documents = service.getAllUsers();
 		documents.stream().forEach(doc -> {
-			System.out.println(doc.content());
+			logger.info(doc.content());
 		});
+
+		ConnectionManager.closeBucket(bucket);
 	}
 }
--- src/main/java/com/kd/example/couch/connection/ConnectionManager.java
+++ src/main/java/com/kd/example/couch/connection/ConnectionManager.java
@@ -21,7 +21,7 @@ public class ConnectionManager {
 	/**
 	 * Using default bucket for now.
 	 */
-	public static Bucket getCouchbaseClient(String bucketname,boolean createnew, String... nodes) {
+	public static Bucket getCouchbaseClient(String bucketname, boolean createnew, String... nodes) {
 		// Simple Cluster, connected to localhost and default bucket
 		Cluster cluster = CouchbaseCluster.create();
 		Bucket defaultBucket = cluster.openBucket();
@@ -41,14 +41,15 @@ public class ConnectionManager {
 		Observable.just(cluster11, cluster12).subscribe();
 
 		checkAndCreateBucket(bucketname, cluster1, createnew);
-		
+
 		Bucket specificBucket = cluster1.openBucket(bucketname);
-		
+
 		return specificBucket;
 	}
 
 	/**
 	 * Check if bucket exists, if not create new bucket with specified name
+	 * 
 	 * @param bucketname
 	 * @param cluster1
 	 */
@@ -61,23 +62,44 @@ public class ConnectionManager {
 		}
 	}
 
+	/**
+	 * Close bucket and release resources when not required.
+	 * 
+	 * @param bucket
+	 */
+	public static void closeBucket(Bucket bucket) {
+		if (bucket != null && !bucket.isClosed()) {
+			bucket.async().close();
+		}
+	}
+
+	/**
+	 * Close all open clusters and environment
+	 * 
+	 * @param env
+	 * @param clusters
+	 */
 	public static void shutdownClusterConnection(CouchbaseEnvironment env, List<CouchbaseCluster> clusters) {
 		// If environment is used to create cluster, we need to shutdown ourself
-
-		Observable.from(clusters).flatMap(new Func1<CouchbaseCluster, Observable<? extends Boolean>>() {
-			@Override
-			public Observable<? extends Boolean> call(CouchbaseCluster c) {
-				return Observable.just(c.disconnect());
-			}
-		}).last().flatMap(new Func1<Boolean, Observable<? extends Boolean>>() {
-			@Override
-			public Observable<? extends Boolean> call(Boolean isDisconnected) {
-				if (isDisconnected) {
-					isDisconnected = env.shutdown();
+		if (clusters != null && !clusters.isEmpty()) {
+			Observable.from(clusters).flatMap(new Func1<CouchbaseCluster, Observable<? extends Boolean>>() {
+				@Override
+				public Observable<? extends Boolean> call(CouchbaseCluster c) {
+					return Observable.just(c.disconnect());
 				}
-				// shutdown environment when last cluster has disconnected
-				return Observable.just(isDisconnected);
-			}
-		});
+			}).last().flatMap(new Func1<Boolean, Observable<? extends Boolean>>() {
+				@Override
+				public Observable<? extends Boolean> call(Boolean isDisconnected) {
+					if (isDisconnected) {
+						if (env != null) {
+							isDisconnected = env.shutdown();
+						}
+					}
+					// shutdown environment when last cluster has disconnected
+					return Observable.just(isDisconnected);
+				}
+			});
+		}
+
 	}
 }
